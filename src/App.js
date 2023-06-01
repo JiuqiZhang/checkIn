a1ed3d4204f9fafb6cdc2927324b76d174560c39
JiuqiZhang <jz3337@nyu.edu>
2023-06-01 06:03:13 -0400

    update readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import ticket from './ticket.jpg'
+function Copyright(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://www.instagram.com/9ussell7hang/">
+        9ussell7hang
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
 }
 
-export default App;
+const defaultTheme = createTheme();
+
+export default function App() {
+  const [letter, setletter] =  React.useState(true)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    if (data.get("email").toLowerCase() === 'roxy lin' && data.get("password")==='230602'){
+      console.log("Signed");
+      setletter(false)
+    }else{
+      if (data.get("email").toLowerCase() !== 'roxy lin'){
+        alert("Full Name doesn't match.")
+      }else if(data.get("password") !=='230602'){
+        alert("Verification Code doesn't match.")
+      }else{
+        alert("Full Name and Verification Code doesn't match.")
+      }
+      
+    }
+    
+  };
+
+  if (letter){
+    return (
+      <ThemeProvider theme={defaultTheme}>
+        <Grid container component="main" sx={{ height: "100vh" }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage:
+                "url(https://source.unsplash.com/random?wallpapers)",
+              backgroundRepeat: "no-repeat",
+              backgroundColor: (t) =>
+                t.palette.mode === "light"
+                  ? t.palette.grey[50]
+                  : t.palette.grey[900],
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                信件查询
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Full Name"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Verification Code"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  autoFocus
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link
+                      href="https://www.instagram.com/9ussell7hang/"
+                      variant="body2"
+                    >
+                      Need to contact support team?
+                    </Link>
+                  </Grid>
+                </Grid>
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
+    );
+  }else{
+    return(<Box sx={{ m: "4%"}} ><h1>Hey Roxy,</h1>
+    <h3>I would like to invite you to a museum trip this Friday 6:00pm. Hope to see you again.<br/>
+
+Looking forward to hearing from you soon.</h3>
+
+
+<h3>Kind regards,</h3>
+
+<h2>Russell Zhang</h2>
+<br/><br/><Box sx={{ m: 2,}} display="flex" flexDirection='column'
+    justifyContent="center"
+    alignItems="center">
+
+<img src={ticket}/>
+   
+    </Box></Box>)
+  }
+}
